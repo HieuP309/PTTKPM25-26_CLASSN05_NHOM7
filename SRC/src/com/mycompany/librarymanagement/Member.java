ab8c662620173bfd2223130e2387adf1ffc7605a
HoanGBoizzz <121919752+HoanGBoizzz@users.noreply.github.com>
2025-09-04 10:39:03 +0700

    Update Member.java

--- SRC/src/com/mycompany/librarymanagement/Member.java
+++ SRC/src/com/mycompany/librarymanagement/Member.java
@@ -1,1 +1,25 @@
+package com.mycompany.librarymanagement;
 
+public class Member {
+    private String memberId;
+    private String name;
+    private String email;
+
+    public Member(String memberId, String name, String email) {
+        this.memberId = memberId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void borrowBook(Book book) {
+        book.checkOut();
+    }
+
+    public void returnBook(Book book) {
+        book.returnBook();
+    }
+}
